2921c0bd9c6d16fcf4218e02499a8dbc34f58fb1
Joe Danica Inigo <joedanica.inigo@unc.edu>
2022-09-26 15:07:18 -0400

    EX05 Progress

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,6 +2,7 @@
 
 __author__ = "730471018"
 
+
 def only_evens(numbers_list: list[int]) -> list[int]:
     """Only the even numbers from the original list will be returned as a new list of integers."""
     i: int = 0
@@ -11,6 +12,7 @@ def only_evens(numbers_list: list[int]) -> list[int]:
             evens_list.append(numbers_list[i])
         i += 1
     return evens_list
+
 
 def concat(first_list: list[int], second_list: list[int]) -> list[int]:
     """A new list should be generated with the integers of the first list followed by the integers of the second list."""
@@ -24,6 +26,7 @@ def concat(first_list: list[int], second_list: list[int]) -> list[int]:
         concat_list.append(second_list[idx])
         idx += 1
     return concat_list
+
 
 def sub(integer_list: list[int], start_index: int, end_index: int) -> list[int]:
     """A subset of a given list of integers within a starting and ending index (not inclusive) will be returned."""
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -6,20 +6,24 @@ from utils import only_evens, sub, concat
 
 # tests for only_evens
 
+
 def test_only_evens_even_items_1() -> None:
     """Ensures that the one even number in the list (2) is identified and added to the new list."""
     items: list[int] = [1, 2, 3]
     assert only_evens(items) == [2]
+
 
 def test_only_evens_even_items_2() -> None:
     """Ensures that in a list of all even numbers, all of the numbers are added to the new list."""
     items: list[int] = [4, 4, 4]
     assert only_evens(items) == [4, 4, 4]
 
+
 def test_only_evens_even_items_3() -> None:
     """Ensures that in a longer sequence, the 2 even numbers (single and double-digit) are identified and added to the new list."""
     items: list[int] = [1, 3, 8, 10, 5]
     assert only_evens(items) == [8, 10]
+
 
 def test_only_evens_no_even() -> None:
     """Ensures that when no even numbers are present, an empty list is generated (edge case)."""
@@ -28,11 +32,13 @@ def test_only_evens_no_even() -> None:
 
 # tests for concat
 
+
 def test_concat_empty_list_1() -> None:
     """If first list is empty, continues to append integers of second list w/o modification (edge case)."""
     list_1: list[int] = list()
     list_2: list[int] = [1, 2, 3]
     assert concat(list_1, list_2) == [1, 2, 3]
+
 
 def test_concat_empty_list_2() -> None:
     """If second list is empty, continues to append integers of first list w/o modification (edge case)."""
@@ -40,11 +46,13 @@ def test_concat_empty_list_2() -> None:
     list_2: list[int] = list()
     assert concat(list_1, list_2) == [4, 5, 6]
 
+
 def test_concat_equal_list() -> None:
     """If lists of equal lengths are given, new list is made without modifications to integers."""
     list_1: list[int] = [7, 8, 9]
     list_2: list[int] = [10, 11, 12]
     assert concat(list_1, list_2) == [7, 8, 9, 10, 11, 12]
+
 
 def test_concat_unequal_list() -> None:
     """If lists of unequal lengths are given, new list is made without modifications to integers."""
@@ -54,12 +62,14 @@ def test_concat_unequal_list() -> None:
 
 # tests for sub
 
+
 def test_sub_same_index() -> None:
     """If start and end indexes are the same, an empty list should be returned (edge case)."""
     int_list: list[int] = [1, 2, 3, 4]
     start_i: int = 2
     end_i: int = 2
     assert sub(int_list, start_i, end_i) == []
+
 
 def test_sub_empty_list() -> None:
     """Ensures empty list is returned if list has no values."""
@@ -68,12 +78,14 @@ def test_sub_empty_list() -> None:
     end_i: int = 0
     assert sub(int_list, start_i, end_i) == []
 
+
 def test_sub_incorrect_indexes() -> None:
     """Ensures that when incorrect start and end indexes are given, they are fixed to result in all values of the list being returned."""
     int_list: list[int] = [1, 2, 3, 4]
     start_i: int = -1
     end_i: int = 5
     assert sub(int_list, start_i, end_i) == [1, 2, 3, 4]
+
 
 def test_sub_greater_start() -> None:
     """Ensures that if start index is greater than the length of the list, an empty list is returned."""
@@ -82,6 +94,7 @@ def test_sub_greater_start() -> None:
     end_i: int = 2
     assert sub(int_list, start_i, end_i) == []
 
+
 def test_sub_zero_end() -> None:
     """Ensures that if end index is at most 0, an empty list is returned."""
     int_list: list[int] = [1, 2, 3, 4]
@@ -89,12 +102,14 @@ def test_sub_zero_end() -> None:
     end_i: int = 0
     assert sub(int_list, start_i, end_i) == []
 
+
 def test_sub_values_small() -> None:
     """Ensures that for a small list of numbers, the proper subset is given."""
     int_list: list[int] = [1, 2, 3, 4]
     start_i: int = 1
     end_i: int = 3
     assert sub(int_list, start_i, end_i) == [2, 3]
+
 
 def test_sub_values_large() -> None:
     """Ensures that for a large list of numbers, the proper subset is given."""
