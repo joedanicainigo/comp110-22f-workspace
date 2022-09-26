dbd506fd0d84190535ddd3bbcac38532a010d963
Joe Danica Inigo <joedanica.inigo@unc.edu>
2022-09-26 13:38:59 -0400

    EX04 Progress

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,8 +2,11 @@
 
 __author__ = "730471018"
 
+
 def all(integer_list: list[int], given_integer: int) -> bool:
-    "Check whether a given integer is in a list of integers."
+    """Check whether a given integer is in a list of integers."""
+    if len(integer_list) == 0:
+        return False
     i: int = 0
     while i < len(integer_list):
         if integer_list[i] != given_integer:
@@ -12,8 +15,9 @@ def all(integer_list: list[int], given_integer: int) -> bool:
     return True
     return all
     
+
 def max(int_list: list[int]) -> int:
-    "Given an inputted list of integers, the largest integer will be returned."
+    """Given an inputted list of integers, the largest integer will be returned."""
     if len(int_list) == 0:
         raise ValueError("max() arg is an empty List")
     else:
@@ -23,10 +27,18 @@ def max(int_list: list[int]) -> int:
             if int_list[idx] > max_value:
                 max_value = int_list[idx]
             idx += 1
-    return(max_value)
+    return max_value
+
 
 def is_equal(list_1: list[int], list_2: list[int]) -> bool:
-    "Checks if the elements and the order they are in are identical between 2 lists of integers."
+    """Checks if the elements and the order they are in are identical between 2 lists of integers."""
+    if len(list_1) != len(list_2):
+        return False
+    if len(list_1) == 0 and len(list_2) == 0:
+        return True
+    else: 
+        if len(list_1) == 0 or len(list_2) == 0:
+            return False
     index_1: int = 0
     index_2: int = 0
     while index_1 < len(list_1) and index_2 < len(list_2):
